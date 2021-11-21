6392efb81f8a491255ee06f65ea8b9abd156a420
Hemerson G.Ramiro Jr <61366887+Jamer05@users.noreply.github.com>
2021-11-21 18:48:55 +0800

    update

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@ g="\033[1;32m"
 r="\033[1;31m"
 b="\033[1;34m"
 w="\033[0m"
-echo "test"
-echo -e $b">"$w"  J05-Team-POS - For foodchain services"
+echo -e $b">"$w" J05-Team-POS - For foodchain services"
 echo -e $b">"$w" prepare for installing dependencies ..."
 sleep 3
 echo -e $b">"$w" Cloning J05-Team-POS "$g"MyCaseStudy"$w
