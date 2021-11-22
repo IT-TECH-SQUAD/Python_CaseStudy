3ab82150b88bb5c1e38975bb71e6ca12d57c2c71
Hemerson G.Ramiro Jr <61366887+Jamer05@users.noreply.github.com>
2021-11-22 20:15:08 +0800

    update

--- install.bat
+++ install.bat
@@ -8,6 +8,7 @@ echo [STARTING]Prepare for installing dependencies
 timeout /t 3 /nobreak > NUL
 echo [INSTALLING]File is downloading
 curl https://github.com/Jamer05/Python_CS/archive/refs/heads/main.zip -L -o Python_CS.zip && tar -xf Python_CS.zip && cd Python_CS-main
+cd Python_CS-main
 echo [Project has been cloned]
 echo [INSTALLIING]Downloading resources...
 pip install -r requirements.txt
